ce72d3f22ec11503fe4952ba28d58237522b4afb
Alex Munro <alexmunro91@gmail.com>
2021-12-05 01:49:12 +0000

    Fix API documentation and re-arrange CLI file

--- lib/advent_of_code_cli.rb
+++ lib/advent_of_code_cli.rb
@@ -2,25 +2,25 @@
 
 require "thor"
 
-require_relative "./initialiser"
-require_relative "./preparer"
-require_relative "./submitter"
+require_relative "advent_of_code_cli/initialiser"
+require_relative "advent_of_code_cli/preparer"
+require_relative "advent_of_code_cli/submitter"
 
 module AdventOfCodeCLI
   # Commands that can be used to interact with this Advent of Code CLI
-  class Cli < Thor
+  class AdventOfCodeCLI < Thor
     desc "init", "initialise config including your AoC session key and year in this directory"
     def init
       Initialiser.init_aoc
     end
 
-    desc "prepare --day", "prepare input and solution files for the given day (number from 1-25)"
+    desc "prepare", "prepare input and solution files for the given day (number from 1-25)"
     option :day, required: true, type: :numeric, aliases: [:d]
     def prepare
       Preparer.prep_for(options[:day])
     end
 
-    desc "submit --day --part [ANSWER]", "submit a solution for a given day and part"
+    desc "submit [ANSWER]", "submit a solution for a given day and part"
     option :day, required: true, type: :numeric, aliases: [:d]
     option :part, required: true, type: :numeric, aliases: [:p]
     def submit(answer)
@@ -29,4 +29,4 @@ module AdventOfCodeCLI
   end
 end
 
-AdventOfCodeCLI::Cli.start(ARGV)
+AdventOfCodeCLI::AdventOfCodeCLI.start(ARGV)
